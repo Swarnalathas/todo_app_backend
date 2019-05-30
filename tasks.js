34a77328e0320e72ed23323a5e6f502929b43ca1
Swarna <swarna.prakashm@gmail.com>
2019-05-30 10:55:18 +0100

    Added task array

--- tasks.js
+++ tasks.js
@@ -7,7 +7,13 @@ app.get("/tasks",function (request,response) {
   const username = request.query.username;
   response.json({
     //message :"Hello from my tasks from Lambda function"
-    message : `Username ${username} requested the tasks`
+    //message : `Username ${username} requested the tasks`
+    tasks:[
+      { Description :"Walk dog",TaskId: 1 , Completed : true , UserId:1},
+      { Description :"Buy Suger",TaskId: 2 , Completed : false , UserId:1},
+      { Description :"Clean Vessel",TaskId: 3 , Completed : false , UserId:1},
+      { Description :"Swimming",TaskId: 4 , Completed : true , UserId:1}
+    ]
   });
 });
 
