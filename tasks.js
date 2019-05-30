e66c2f50aefc75ea22b008492b7059a01a5ae682
Swarna <swarna.prakashm@gmail.com>
2019-05-30 13:17:32 +0100

    set the delete the request

--- tasks.js
+++ tasks.js
@@ -10,19 +10,36 @@ const connection  = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
-connection.connect();
+//connection.connect();
 
 app.get("/tasks",function (request,response) {
   connection.query("SELECT * FROM tasks",function(err,result,fields) { 
     if(err !== null) {
       //for debugging purposes
       console.log("Error fetching tasks",err);
-      //Response to the end client with suitable response
-      //response.status(500).json({ message : "Something went wrong" });
+      //Response to the end client with suitable response      
       response.send(500); //response code 500 means user error
-    }
-    response.json({ tasks: result});
+    } else {
+      response.json({ tasks: result});
+    }   
   });  
+});
+
+app.delete("/tasks/:id",function(request,response){
+  const taskId = request.params.id;
+  console.log(taskId);
+  connection.query("DELETE FROM tasks WHERE task_id = ?",[taskId],function(err,result,fields){
+    if(err !== null) {
+      console.log("something went wrond deleting the task",err );
+      response.send(500);
+    } else {
+    response.send("Item Deleted");
+    }
+  });
+  //delete the task from database 
+  //respond with a 200 status code
+ 
+
 });
 
 module.exports.handler = serverless(app);
